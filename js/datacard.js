9acc974675ab184eedac45f5f522163a462c48a3
cerasus04 <cerasus1004@gmail.com>
2020-10-18 14:32:20 +0300

    Убрала комментарии

--- js/datacard.js
+++ js/datacard.js
@@ -58,42 +58,6 @@
 
   getCards();
 
-  // const renderPin = (obj) => {
-  //   const pinTemplate = document.querySelector(`#pin`).content.querySelector(`.map__pin`);
-  //   const pin = pinTemplate.cloneNode(true);
-  //   const imgEl = pin.querySelector(`img`);
-  //   pin.style.left = `${obj.location.x - PIN.WIDTH / 2}px`;
-  //   pin.style.top = `${obj.location.y - (PIN.HEIGHT + PIN.MARKER_HEIGHT)}px`;
-  //   imgEl.src = obj.author.avatar;
-  //   imgEl.alt = obj.offer.title;
-
-  //   pin.addEventListener(`click`, function (evt) {
-  //     setPinActiveClass((evt.target.tagName === `IMG`) ? evt.target.parentElement : evt.target);
-  //     closePopup();
-  //     createCard(obj);
-  //   });
-
-  //   return pin;
-  // };
-
-  // const setPinActiveClass = (button) => {
-  //   let activePin = mapPins.querySelector(`.map__pin--active`);
-
-  //   if (activePin) {
-  //     activePin.classList.remove(`map__pin--active`);
-  //   }
-
-  //   button.classList.add(`map__pin--active`);
-  // };
-
-  // const createPins = () => {
-  //   const fragment = document.createDocumentFragment();
-  //   for (let pin of cards) {
-  //     fragment.appendChild(renderPin(pin));
-  //   }
-  //   mapPins.appendChild(fragment);
-  // };
-
   window.datacard = {
     cards: cards
   };
